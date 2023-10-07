23a9d80f84efce62fcb57962361e05f91289694e
pderer <capospi9903@naver.com>
2023-10-08 02:36:33 +0900

    삼성 기출(인구 이동) - BFS - visited 리스트의 값을 연합별로 다르게 함으로써 인구수를 연합별로 다르게 계산가능하도록 했음 - 리스트 초기화를 -1로 해서 처리 유무를 표현 - index 값으로 while문 탈출 조건 명시

--- python/16234.py
+++ python/16234.py
@@ -2,22 +2,19 @@ from collections import deque
 
 
 n, l, r = map(int, input().split())  # noqa: E741
-visited = [[0] * n for _ in range(n)]
+visited = [[-1] * n for _ in range(n)]
 
 land = [] # 땅
 for i in range(n):
     land.append(list(map(int, input().split())))
 
-# print(visited)
-# print(land)
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
+def bfs(x, y, index):
     queue = deque()
     queue.append((x, y))
-    visited[x][y] = 1 # 연합 표시
+    visited[x][y] = index # 연합 표시
     union = land[x][y]
     count = 1
     while queue:
@@ -28,40 +25,29 @@ def bfs(x, y):
             if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
             temp = abs(land[temp_x][temp_y] - land[nx][ny])
-            if temp >= l and temp <= r and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
+            if temp >= l and temp <= r and visited[nx][ny] == -1:
+                visited[nx][ny] = index
                 queue.append((nx, ny))
                 union += land[nx][ny]
                 count += 1
     if count > 1:
         temp = int(union / count)
-        print(temp, union, count)
         for i in range(n):
             for j in range(n):
-                if visited[i][j] == 1:
+                if visited[i][j] == index:
                     land[i][j] = temp
-        print(land)
-    else:
-        visited[x][y] = 0 # 연합 실패
-        return False
 
 count = 0
 while True:
-    check = False
+    index = 0
     for i in range(n):
         for j in range(n):
-            print(i, j)
-            bfs(i, j)
-
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j] == 1:
-                check = True
-                break
-    if check is False:
+            if visited[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
+                bfs(i, j, index)
+                index += 1
+    if index == n * n:
         break
-    else:
-        visited = [[0] * n for _ in range(n)] # 초기화 후 다시 bfs
-        count += 1
+    visited = [[-1] * n for _ in range(n)] # 초기화 후 다시 bfs
+    count += 1
 
 print(count)
