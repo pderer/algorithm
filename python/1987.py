55b0005bdbc956d1a0df40cfd3337c9b7804a51c
pderer <capospi9903@naver.com>
2023-10-21 21:17:45 +0900

    백트래킹 (DFS) - 알파벳 별로 check list 생성 - visited list로 체크하는 것과 비슷함 - dfs 경로 별로 탐색을 마쳤을 경우에 그 방향을 다시 갈 수도 있기 때문에 다시 0으로 초기화 해주는 것이 포인트 - 처음 1행 1열은 체크상태, cnt도 1

--- python/1987.py
+++ python/1987.py
@@ -6,19 +6,21 @@ for _ in range(r):
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-answer = 1
-def bfs(x, y):
+check = [False] * 26
+answer = 0
+def dfs(x, y, cnt):
     global answer
-    q = set([(x, y, alphabet[x][y])])
-    while q:
-        x, y, ans = q.pop()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or ny < 0 or nx >= r or ny >= c:
-                continue
-            if alphabet[nx][ny] not in ans:
-                q.add(nx, ny, ans + alphabet[nx][ny])
-                answer = max(answer, len(ans) + 1)
+    answer = max(cnt, answer)
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or ny < 0 or nx >= r or ny >= c:
+            continue
+        if check[ord(alphabet[nx][ny]) - 65] is False:
+            check[ord(alphabet[nx][ny]) - 65] = True
+            ncnt = cnt + 1
+            dfs(nx, ny, ncnt)
+            check[ord(alphabet[nx][ny]) - 65] = False
 
-bfs(0, 0)
+check[ord(alphabet[0][0]) - 65] = True
+dfs(0, 0, 1)
 print(answer)
